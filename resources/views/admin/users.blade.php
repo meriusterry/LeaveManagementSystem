3bf9bfbbf1a67e8e5a8380a5d667f86679de79b9
Terry Ntuli <meriusterry>
2025-03-20 07:04:51 +0200

    #addited a heading

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -25,7 +25,7 @@
                                 <tr>
                                     <th scope="col"
                                         class="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider">
-                                        Employee#</th>
+                                        Employee ID</th>
                                     <th scope="col"
                                         class="px-6 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider">
                                         Employee Name</th>
